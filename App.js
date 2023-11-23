f502c1595b0bf1c4ee8b6b357fcdb79cbe71dbae
Cesar Augusto <augustocsar22@gmail.com>
2023-11-23 06:54:52 -0300

    criando tabela notas

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 import { FlatList, SafeAreaView, StatusBar, StyleSheet } from "react-native"
 import NotaEditor from "./src/componentes/NotaEditor"
-import AsyncStorage from "@react-native-async-storage/async-storage"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Nota } from "./src/componentes/Nota";
+import { criaTabela } from "./src/servicos/Notas";
 
 export default function App() {
+
+  useEffect(() => {
+    criaTabela()
+  }, []);
 
   const [notas, setNotas] = useState([]);
 
   async function mostraNotas() {
-    const todasChaves = await AsyncStorage.getAllKeys();
-    const todasNotas = await AsyncStorage.multiGet(todasChaves);
     setNotas(todasNotas);
     console.log(notas);
 
